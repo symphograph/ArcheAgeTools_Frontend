55086404e1de474f07643b402fd04390985149ac
symphograph <58216294+symphograph@users.noreply.github.com>
2023-01-10 13:39:11 +1100

    fix groupCraft

--- src/components/craft/CraftInfo.vue
+++ src/components/craft/CraftInfo.vue
@@ -2,11 +2,25 @@
   <div class="CraftInfo" v-if="Craft">
     <div>
       <q-list dense>
+        <q-item>
+          <q-item-section>
+            Craft Id:
+          </q-item-section>
+          <q-item-section side>
+            <q-btn :label="Craft.id"
+                   icon-right="content_copy"
+                   class="no-padding"
+                   dense flat
+                   @click="copy(Craft.id)"
+            >
+            </q-btn>
+          </q-item-section>
+        </q-item>
         <q-item dense>
           <q-item-section avatar>
             <ItemIcon :grade="Craft.Prof.lvl"
                       :locIcon="'/img/profs/' + Craft.Prof.id + '.png'"
-                      :tool-text="Craft.Prof.name"
+                      :tool-text="'Профессия'"
                       :size="'30px'"></ItemIcon>
           </q-item-section>
           <q-item-section>
@@ -20,7 +34,7 @@
           <q-item-section avatar>
             <ItemIcon :grade="1"
                       :locIcon="'/img/interface/doodad.png'"
-                      :tool-text="Craft.doodName"
+                      :tool-text="'Приспособление'"
                       :size="'30px'"></ItemIcon>
           </q-item-section>
           <q-item-section>
@@ -118,6 +132,9 @@
 import {computed, inject, ref} from "vue";
 import ItemIcon from "components/ItemIcon.vue"
 import {priceImager} from "src/myFuncts.js"
+import {copyToClipboard, useQuasar} from "quasar";
+
+const q = useQuasar()
 
 const props = defineProps({
   Craft: ref(null)
@@ -138,6 +155,22 @@ const profit = computed(()=> {
   return price - props.Craft.countData.craftCost
 
 })
+
+function copy (val) {
+  copyToClipboard(val)
+    .then(() => {
+      q.notify({
+        color: 'positive',
+        position: 'center',
+        message: 'Скопировано',
+        icon: 'content_copy',
+        timeout: 1
+      })
+    })
+    .catch(() => {
+      // fail
+    })
+}
 
 function profNeed(need)
 {
